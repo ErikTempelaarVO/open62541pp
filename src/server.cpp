a2cae413f767ea5e26309fc658954877d0440bc3
Alexander Karzhenkov <karzhenkov@mail.ru>
2026-07-14 12:34:43 +0500

    fix: prevent UA_Server_run_iterate from being called concurrently with UA_Server_run_shutdown (#717)

--- src/server.cpp
+++ src/server.cpp
@@ -401,9 +401,8 @@ static void runStartup(Server& server, detail::ServerContext& context) {
 }
 
 uint16_t Server::runIterate() {
-    if (!context().running) {
-        runStartup(*this, context());
-    }
+    runStartup(*this, context());
+    const std::scoped_lock runLock{context().mutexRun};
     if (!context().running) {
         return 0;
     }
@@ -414,9 +413,6 @@ uint16_t Server::runIterate() {
 
 void Server::run() {
     runStartup(*this, context());
-    if (!context().running) {
-        return;
-    }
     const std::scoped_lock runLock{context().mutexRun};
     try {
         while (context().running) {
